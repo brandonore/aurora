8d920d526409ef0e8c03619949b1356c74000bb4
Brandon Ore <y2ktheory@gmail.com>
2017-12-26 19:10:51 -0700

    Removed files, minor tweaks

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 400, height: 200, frame: false, transparent: true, icon: path.join(__dirname, 'assets/icons/png/64x64.png')})
+  win = new BrowserWindow({width: 400, height: 200, frame: false, transparent: true})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
